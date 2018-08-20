0c7c392f2505dc6cf2ddc270a7d16d9c0cbbb1ce
mualphatheta <mualphatheta@darienps.org>
2018-08-20 13:27:16 -0400

    Updating website on Mon Aug 20 13:27:16 EDT 2018

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
+git submodule init
+git submodule update
 wget https://github.com/gohugoio/hugo/releases/download/v0.47/hugo_0.47_Linux-64bit.deb -O /tmp/hugo.deb
 sudo dpkg -i /tmp/hugo.deb
